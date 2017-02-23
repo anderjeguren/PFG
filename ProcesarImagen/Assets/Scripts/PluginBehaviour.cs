c97361c20cb9b4e23d5a4b0bf24de221ca96da65
anderjeguren <anderjeguren@gmail.com>
2017-02-23 13:40:04 +0100

    23/02/2017

---
+++ ProcesarImagen/Assets/Scripts/PluginBehaviour.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+internal class PluginBehaviour : MonoBehaviour {
+
+    struct ImageData
+    {
+        IntPtr cvMat;
+    }
+
+    [DllImport("NativeDll")]
+    static extern void testFunction();
+
+    [DllImport("NativeDll")]
+    static extern bool openVideoCapture(int value);
+
+    [DllImport("NativeDll")]
+    static extern ImageData cvtColor(ref ImageData ptr);
+
+    [DllImport("NativeDll")]
+    static extern ImageData createFromExternal(byte[] buffer, int rows, int cols);
+
+    [DllImport("NativeDll")]
+    static extern ImageData captureFrame();
+
+    [DllImport("NativeDll")]
+    static extern void gaussianBlur(ref ImageData ptr);
+
+    [DllImport("NativeDll")]
+    static extern void canny(ref ImageData ptr);
+
+    [DllImport("NativeDll")]
+    static extern bool imageShow(ref ImageData ptr, String caption);
+
+    [DllImport("NativeDll")]
+    static extern void releaseImage(ref ImageData ptr);
+
+    /// <summary>
+    /// The webcam texture.
+    /// </summary>
+    private static byte[] buffer;
+
+    // Use this for initialization
+    static void Initialize(WebCamTexture webCamTexture)
+    {
+        buffer = new byte[webCamTexture.height * webCamTexture.width * 3];
+    }
+
+    static void WebCamToByteArray(WebCamTexture webCamTexture, byte[] data)
+    {
+        Color32[] colors = webCamTexture.GetPixels32();
+        for (int i = 0; i < colors.Length; i++)
+        {
+            data[(i * 3)] = colors[i].r;
+            data[(i * 3) + 1] = colors[i].g;
+            data[(i * 3) + 2] = colors[i].b;
+        }
+    }
+
+    internal static void ExecuteBlur(WebCamTexture webCamTexture)
+    {
+        Initialize(webCamTexture);
+        WebCamToByteArray(webCamTexture, buffer);
+        var img = createFromExternal(buffer, webCamTexture.height, webCamTexture.width);
+        var gray = cvtColor(ref img);
+        gaussianBlur(ref gray);
+        canny(ref gray);
+        if (!imageShow(ref gray, "Hello world!"))
+        {
+            releaseImage(ref gray);
+            releaseImage(ref img);
+        }
+        releaseImage(ref gray);
+        releaseImage(ref img);
+    }
+}
--- ProcesarImagen/Assets/Scripts/TextureController.cs
+++ ProcesarImagen/Assets/Scripts/TextureController.cs
@@ -1,59 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Runtime.InteropServices;
+﻿using UnityEngine;
 
 
 public class TextureController : MonoBehaviour
 {
-    struct ImageData
-    {
-        IntPtr cvMat;
-    }
-
-    [DllImport("NativeDll")]
-    static extern void testFunction();
-
-    [DllImport("NativeDll")]
-    static extern bool openVideoCapture(int value);
-
-    [DllImport("NativeDll")]
-    static extern ImageData cvtColor(ref ImageData ptr);
-
-    [DllImport("NativeDll")]
-    static extern ImageData createFromExternal(byte[] buffer, int rows, int cols);
-
-    [DllImport("NativeDll")]
-    static extern ImageData captureFrame();
-
-    [DllImport("NativeDll")]
-    static extern void gaussianBlur(ref ImageData ptr);
-
-    [DllImport("NativeDll")]
-    static extern void canny(ref ImageData ptr);
-
-    [DllImport("NativeDll")]
-    static extern bool imageShow(ref ImageData ptr, String caption);
-
-    [DllImport("NativeDll")]
-    static extern void releaseImage(ref ImageData ptr);
-
     /// <summary>
     /// The webcam texture.
     /// </summary>
     private WebCamTexture webCamTexture;
-
-    /// <summary>
-    /// Buffer used to transfer info to the plugin
-    /// </summary>
-    private byte[] data;
-
-    // Use this for initialization
-    void Initialize()
-    {
-        data = new byte[webCamTexture.height * webCamTexture.width * 3];
-    }
 
     // Use this for initialization
     void Start()
@@ -73,38 +26,8 @@ public class TextureController : MonoBehaviour
 
         if (webCamTexture.isPlaying)
         {
-            //First version blur image
-            ExecuteBlur(webCamTexture);
+            //First version blur image         
+            PluginBehaviour.ExecuteBlur(webCamTexture);
         }
     }
-
-
-    void WebCamToByteArray(WebCamTexture webCamTexture, byte[] buffer)
-    {
-        Color32[] colors = webCamTexture.GetPixels32();
-        for (int i = 0; i < colors.Length; i++)
-        {
-            buffer[(i * 3)] = colors[i].r;
-            buffer[(i * 3) + 1] = colors[i].g;
-            buffer[(i * 3) + 2] = colors[i].b;
-        }
-    }
-
-    void ExecuteBlur(WebCamTexture webCamTexture)
-    {
-        Initialize();
-        WebCamToByteArray(webCamTexture, data);
-        var img = createFromExternal(data, webCamTexture.height, webCamTexture.width);
-        var gray = cvtColor(ref img);
-        gaussianBlur(ref gray);
-        canny(ref gray);
-        if (!imageShow(ref gray, "Hello world!"))
-        {
-            releaseImage(ref gray);
-            releaseImage(ref img);
-        }
-        releaseImage(ref gray);
-        releaseImage(ref img);
-    }
-
 }
